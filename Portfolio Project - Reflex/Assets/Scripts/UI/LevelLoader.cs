64d421ee4b249eb5f7ba82d4bbbf50e55ead61c4
Calum <43376276+Calum532@users.noreply.github.com>
2020-05-03 07:25:13 +0100

    Continued designing the main menu

---
+++ Portfolio Project - Reflex/Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public GameObject loadingScreenTip;
+    public Slider slider;
+    public TextMeshProUGUI progressText;
+    private int RandomNum;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+        Time.timeScale = 1f;
+        Pause_Menu.gameIsPaused = false;
+    }
+
+    public void DisplayRandomTip()
+    {
+        RandomNum = UnityEngine.Random.Range(0, 6);
+
+        if (RandomNum == 0)
+        {
+            loadingScreenTip.GetComponent<TextMeshProUGUI>().text = "Tip: 111111111111111111111111111111111111111111111111";
+        }
+        else if (RandomNum == 1)
+        {
+            loadingScreenTip.GetComponent<TextMeshProUGUI>().text = "Fact: 222222222222222222222222222222222222222222222222";
+        }
+        else if (RandomNum == 2)
+        {
+            loadingScreenTip.GetComponent<TextMeshProUGUI>().text = "Tip: 333333333333333333333333333333333333333333333333";
+        }
+        else if (RandomNum == 3)
+        {
+            loadingScreenTip.GetComponent<TextMeshProUGUI>().text = "Fact: 444444444444444444444444444444444444444444444444";
+        }
+        else if (RandomNum == 4)
+        {
+            loadingScreenTip.GetComponent<TextMeshProUGUI>().text = "Tip: 555555555555555555555555555555555555555555555555";
+        }
+        else if (RandomNum == 5)
+        {
+            loadingScreenTip.GetComponent<TextMeshProUGUI>().text = "Fact: 666666666666666666666666666666666666666666666666";
+        }
+        else if (RandomNum == 6)
+        {
+            loadingScreenTip.GetComponent<TextMeshProUGUI>().text = "Tip: 777777777777777777777777777777777777777777777777";
+        }
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = "LOADING..." + (progress * 100f).ToString("0") + "%";
+            yield return null;
+        }
+    }
+}
---
+++ Portfolio Project - Reflex/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject TitleScreenUI;
+    public GameObject MainMenuUI;
+    public GameObject SingleplayerUI;
+    public GameObject MultiplayerUI;
+    public GameObject ControlsUI;
+    public GameObject SettingsUI;
+    public GameObject AreYouSureQuitUI;
+
+    bool viewingTitleScreen = true;
+
+    public void DisableTitleScreen()
+    {
+        if(Input.GetKeyDown(KeyCode.KeypadEnter) && viewingTitleScreen == true)
+        {
+            TitleScreenUI.SetActive(false);
+            viewingTitleScreen = false;
+            MainMenuUI.SetActive(true);
+            //swing the camera round
+        }
+    }
+
+    public void SingleplayerSelect()
+    {
+        SingleplayerUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void MultiplayerSelect()
+    {
+        MultiplayerUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void DisplayControls()
+    {
+        ControlsUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void DisplaySettings()
+    {
+        SettingsUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void Back()
+    {
+        MainMenuUI.SetActive(true);
+        TitleScreenUI.SetActive(false);
+        ControlsUI.SetActive(false);
+        SettingsUI.SetActive(false);
+        SingleplayerUI.SetActive(false);
+        MultiplayerUI.SetActive(false);
+        AreYouSureQuitUI.SetActive(false);
+    }
+
+    public void DisplayAreYouSureQuitUI()
+    {
+        AreYouSureQuitUI.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game...");
+        Application.Quit();
+    }
+}
